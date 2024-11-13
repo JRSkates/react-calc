99a72052a7d76b49470fcbba5ef77673658369d1
JRSkates <skatesproduction@gmail.com>
2024-11-13 18:06:17 +0000

    equals clicker and C clicker handlers built

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -32,39 +32,101 @@ function App() {
       res: 0
   })
 
+// Sign click handler
   function signClickHandler (e) {
-    const typedValue = e.target.value
+    e.preventDefault();
+    const typedValue = e.target.value;
     setCalc((prevCalc) => {
+      console.log('sign click', typedValue);
+      
       return {
         ...prevCalc,
-        sign: typedValue,
-        num: 0,
-        res: prevCalc.num,
+        sign: typedValue,  // Store the operator (+, -, X, /)
+        res: prevCalc.num,  // Store the current number in res
+        num: "",  // Reset num to 0 after operator is clicked
       };
-    })
+    });
   }
-
+  
+  // Number click handler
   function numClickHandler (e) {
-    const typedValue = e.target.value
+    e.preventDefault();
+    const typedValue = e.target.value;
     setCalc((prevCalc) => {
       let newNum;
-      if (prevCalc.num === 0 && typedValue === 0) {
-        newNum = 0;  // Don't allow multiple zeros to be entered
+  
+      if (prevCalc.num === 0 && typedValue === '0') {
+        newNum = '0';  // Don't allow multiple zeros to be entered
       } else if (prevCalc.num === 0) {
         newNum = typedValue;  // Start with the typed value if current num is 0
       } else {
         newNum = prevCalc.num + typedValue;  // Concatenate the typed value to num
       }
-
-      const newRes = prevCalc.sign ? prevCalc.sign : 0;
-
+  
+      const newRes = prevCalc.res;  // Keep the previous result (if any)
+  
+      console.log('num click', newNum);
+      
       return {
         ...prevCalc,
-        num: newNum,
-        res: newRes,
+        num: newNum,  // Update the number
+        res: newRes,  // Keep the previous result
       };
-    })
+    });
   }
+  
+  // Equals click handler
+  function equalsClickHandler (e) {
+    e.preventDefault();
+    setCalc((prevCalc) => {
+      if (prevCalc.sign && prevCalc.num) {
+        let result;
+  
+        // Perform the calculation based on the sign
+        switch (prevCalc.sign) {
+          case '+':
+            result = Number(prevCalc.res) + Number(prevCalc.num);
+            break;
+          case '-':
+            result = Number(prevCalc.res) - Number(prevCalc.num);
+            break;
+          case 'X':
+            result = Number(prevCalc.res) * Number(prevCalc.num);
+            break;
+          case '/':
+            result = Number(prevCalc.res) / Number(prevCalc.num);
+            break;
+          default:
+            result = 0;
+        }
+  
+        console.log('calculated result', result);
+  
+        return {
+          ...prevCalc,
+          num: String(result),  // Update num with the result
+          res: 0,  // Reset res to 0 after calculation
+          sign: '',  // Clear the sign after calculation
+        };
+      } else {
+        return prevCalc;
+      }
+    });
+  }
+
+  function cClickHandler (e) {
+    e.preventDefault();
+    setCalc((prevCalc) => {
+      console.log('C click');
+      return {
+        sign: '',
+        num: 0,
+        res: 0,
+      };
+    });
+  }
+
+
 
   return (
     <>
@@ -76,7 +138,7 @@ function App() {
               key={index} 
               value={value} 
               className={value === "=" ? "equals" : ""}
-              onClick={["+", "-", "X", "/"].includes(value) ? signClickHandler : numClickHandler} // Bind numClickHandler to each button click event
+              onClick={value === "C" ? cClickHandler : value === "=" ? equalsClickHandler : ["+", "-", "X", "/"].includes(value) ? signClickHandler : numClickHandler} // Bind numClickHandler to each button click event
             />
           ))} 
         </div>;
